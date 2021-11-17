93fc5a8880f19ca75e068534b9fdca132c82b490
youcef <mymail>
2021-11-17 16:30:11 +0100

    Index and JS finnished

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 window.addEventListener("load",(e)=>{
     
     let burger = document.getElementById("menu-burger")
+    let navbar = document.getElementById("nav-bar-categories")
+    let isMenuHidden=true
+    // Hide menu, if click in body 
+    document.body.addEventListener("click",(e)=>{
+        if (!isMenuHidden) {
+            burger.classList.toggle("menu-close")
+            isMenuHidden= !isMenuHidden
+            navbar.classList.toggle("hide-this")
+        }
+    })
+    // Toggle menu
     burger.addEventListener("click",(e)=>{
         e.stopPropagation()
-        console.log("tttt")
+        burger.classList.toggle("menu-close")
+        isMenuHidden= !isMenuHidden
+        navbar.classList.toggle("hide-this")
     })
-
 })
